6638fa66f6b982e5a26d77f403f3113f72f33ff2
joegnis <joegnis@pm.me>
2024-10-22 11:00:51 -0700

    Makes parallel helper take into account input amounts

--- src/gtnh/parallel_helper.py
+++ src/gtnh/parallel_helper.py
@@ -1,6 +1,7 @@
 import math
-from dataclasses import KW_ONLY, InitVar, dataclass, field
-from typing import ClassVar
+from dataclasses import KW_ONLY, dataclass, field
+
+from src.data.basicTypes import IngredientCollection
 
 from .overclock_calculator import OverclockCalculator, OverclockCalculatorResult
 
@@ -16,53 +17,53 @@ class ParallelHelperResult:
 @dataclass(eq=False)
 class ParallelHelper:
     """
-    ParallelHelper
+    Calculates the number of parallel and changes to overclocking after parallel.
+
+    Instantiate this class with proper arguments,
+    and call `calculate()` to get results.
+    After that, if we want to change calculation parameters,
+    change public members (those without leading `_`),
+    call public method, and etc,
+    verify the changes by calling `validate()`,
+    and finally call `calculate()` again.
+
+    Design philosophy is similar to that of `OverclockCalculator`.
+
+    A rewrite of the following class in GT5-Unofficial repo:
+    - gregtech/api/util/GT_ParallelHelper.java
+    - GTNH version: 2.6.1
+    - GT5-Unofficial version: 5.09.45.168
+    - GT5-Unofficial Commit: 9ec067dc13f9ef7aff30fcc0ee3244f22bd76dd7
     """
 
-    # Mirrors the following class in Java source code:
-    # - Source: gregtech/api/util/GT_ParallelHelper.java
-    # - GTNH version: 2.6.1
-    # - Commit: 9ec067dc13f9ef7aff30fcc0ee3244f22bd76dd7
     # Only code changed from source is documented.
     # Refer to Java code for more documentation.
+    # Batch mode related code is skipped for now.
 
-    # joegnis:
     # Refer to OverclockCalculator's documents and comments to see
     # what and why I change from Java source code.
 
-    MAX_BATCH_MODE_TICK_TIME: ClassVar[int] = 128
-
     # required arguments in __init__
     calculator: OverclockCalculator
+    ingredient_inputs: IngredientCollection
+    recipe_inputs: IngredientCollection
 
     _: KW_ONLY
     eut_modifier: float = 1.0
     max_parallel: int = 1
 
     # Init-only variables
-    # Complements enable_batch_mode()
-    batch_modifier: InitVar[int] = 1
-
     # Private variables
     _actual_recipe_eut: int = field(init=False)
     _available_eut: int = field(init=False)
     _duration_multiplier: float = field(default=1.0, init=False)
-    _is_batch_mode_on: bool = field(default=False, init=False)
-    _batch_modifier: int = field(default=1, init=False)
     # Removed since it is only used in build()
     # current_parallel: int = -1
     # Removed since it is only used in build()
     # max_parallel: int = 1
 
-    def __post_init__(self, batch_modifier: int) -> None:
-        self.enable_batch_mode(batch_modifier)
+    def __post_init__(self) -> None:
         self.validate()
-        self.build()
-
-    def enable_batch_mode(self, batch_modifier) -> None:
-        if batch_modifier > 1:
-            self._is_batch_mode_on = True
-            self._batch_modifier = batch_modifier
 
     def validate(self) -> None:
         """
@@ -81,10 +82,11 @@ class ParallelHelper:
         Does the calculation.
 
         Call `validate()` before this to validate parameters.
+
+        Equivalent to the methods in Java:
+        - `build()`
+        - `determineParallel()`
         """
-        # Mirrors Java implementation of:
-        # - build()
-        # - determineParallel()
         # Some comments indicate which Java code is skipped
 
         if self.max_parallel <= 0:
@@ -113,8 +115,6 @@ class ParallelHelper:
             max_parallel = int(max_parallel / tick_time_after_oc)
 
         max_parallel_before_batch_mode = max_parallel
-        if self._is_batch_mode_on:
-            max_parallel *= self._batch_modifier
 
         # final ItemStack[] truncatedItemOutputs = recipe.mOutputs != null
         # final FluidStack[] truncatedFluidOutputs = ...
@@ -130,19 +130,11 @@ class ParallelHelper:
                 int(self._available_eut / self._actual_recipe_eut),
             )
 
+        # SingleRecipeCheck is for machines locked into a recipe,
+        # which we should not worry about
         # if (recipeCheck != null)
-        # ...
-        # It assumes that we have infinite input
-        # joegnis:
-        # This condition calls either
-        # - gregtech/api/recipe/check/SingleRecipeCheck.java:checkRecipeInputs()
-        # - gregtech/api/util/GT_Recipe.java:maxParallelCalculatedByInputs()
-        # They both calculate parallel by getting the smallest of
-        # `currentParallel` (argument) and item/fluid amount.
-        # Therefore if we assume we have infinite input,
-        # the result should always be `currentParallel`
-        current_parallel = actual_max_parallel
-
+        current_parallel = self.max_parallel_calculated_by_inputs(actual_max_parallel)
+        # current_parallel = actual_max_parallel
         eut_use_after_oc = self.calculator.calculate_eut_consumption_under_one_tick(
             ORIG_MAX_PARALLEL, current_parallel
         )
@@ -152,31 +144,12 @@ class ParallelHelper:
         # In Java, calculator results might be changed here:
         #     calculator.setRecipeEUt(eutUseAfterOC);
         # Here we don't modify calculator object directly,
-        # but instead we include the changed value in return value
+        # but instead we include the changed values in the return value
         recipe_voltage = res_calc.recipe_voltage
         if current_parallel > ORIG_MAX_PARALLEL:
             recipe_voltage = eut_use_after_oc
 
         duration_multiplier = 1.0
-        if (
-            self._is_batch_mode_on
-            and current_parallel > 0
-            and res_calc.duration < self.MAX_BATCH_MODE_TICK_TIME
-        ):
-            batch_multiplier_max = int(
-                self.MAX_BATCH_MODE_TICK_TIME / res_calc.duration
-            )
-            MAX_EXTRA_PARALLELS = math.floor(
-                min(
-                    current_parallel
-                    * min(batch_multiplier_max - 1, self._batch_modifier - 1),
-                    max_parallel - current_parallel,
-                )
-            )
-            # Skipped recipe check
-            extra_parallels = MAX_EXTRA_PARALLELS
-            duration_multiplier = 1 + extra_parallels / current_parallel
-            current_parallel += extra_parallels
 
         # if (calculateOutputs && currentParallel > 0)
         # ..
@@ -185,4 +158,31 @@ class ParallelHelper:
             recipe_voltage=recipe_voltage,
             duration_multiplier=duration_multiplier,
             calculator_result=res_calc,
+        )
+
+    def max_parallel_calculated_by_inputs(self, max_parallel: int) -> int:
+        """
+        Calculates max parallel by considering `max_parallel` argument,
+        the quantities of input ingredients, and the quantities of recipe input.
+
+        Equivalent to the Java implementation of
+        `GT_Recipe.maxParallelCalculatedByInputs()`.
+        This rewrite does not try to follow the implementation exactly,
+        only simulating its results.
+        In `determineParallel()` in Java, its return value is cast to `int`,
+        so it actually doesn't need to return a double.
+        """
+        # find the smallest input/recipe_input ratio
+        smallest_ratio = math.inf
+        for ing_recipe in self.recipe_inputs:
+            if ing_recipe.name not in self.ingredient_inputs:
+                return 0
+            ing_input = self.ingredient_inputs[ing_recipe.name]
+            recipe_input = self.recipe_inputs[ing_recipe.name]
+            ratio = sum(ing_input) / sum(recipe_input)
+            if ratio < smallest_ratio:
+                smallest_ratio = ratio
+        return min(
+            max_parallel,
+            math.floor(smallest_ratio) if smallest_ratio != math.inf else max_parallel,
         )
--- tests/gtnh/test_parallel_helper.py
+++ tests/gtnh/test_parallel_helper.py
@@ -3,9 +3,15 @@ import math
 import pytest
 from pytest_check import check
 
+from src.data.basicTypes import Ingredient, IngredientCollection
 from src.gtnh.overclock_calculator import OverclockCalculator
 from src.gtnh.parallel_helper import ParallelHelper
 from src.gtnh.values import VoltageTier
+
+recipe_input_ebf_steel_ingot = IngredientCollection(
+    Ingredient("iron dust", 1),
+    Ingredient("oxygen gas", 1000),
+)
 
 
 # Credit to: @Hermanoid, https://github.com/OrderedSet86/gtnh-flow/pull/39
@@ -75,7 +81,16 @@ def test_volcanus_ebf_iron_dust_recipe(
         eut_discount=0.9,
         speed_boost=1.0 / 2.2,
     )
-    helper = ParallelHelper(calculator, eut_modifier=0.9, max_parallel=8)
+    helper = ParallelHelper(
+        calculator,
+        IngredientCollection(
+            Ingredient("iron dust", math.inf),
+            Ingredient("oxygen gas", math.inf),
+        ),
+        recipe_input_ebf_steel_ingot,
+        eut_modifier=0.9,
+        max_parallel=8,
+    )
     res_helper = helper.build()
     res_calc = res_helper.calculator_result
     dur = int(res_helper.duration_multiplier * res_calc.duration)
@@ -87,18 +102,33 @@ def test_volcanus_ebf_iron_dust_recipe(
     check.equal(parallel, expected_parallel)
 
 
+recipe_centrifuge_black_granite = IngredientCollection(
+    Ingredient("black granite dust", 5)
+)
+
+
 @pytest.mark.parametrize(
-    "machine_voltage,expected_eut,expected_dur,expected_parallel",
+    "machine_voltage,input_amount,expected_eut,expected_dur,expected_parallel",
     [
-        (VoltageTier.HV, 162, 178, 18),
-        (VoltageTier.EV, 864, 89, 24),
-        (VoltageTier.UEV, 2_211_840, 2, 60),
-        (VoltageTier.UIV, 9_732_096, 1, 66),
-        (VoltageTier.UMV, 42_467_328, 1, 103),
+        # Just enough input for 18 parallel
+        (VoltageTier.HV, 90, 162, 178, 18),
+        (VoltageTier.HV, 64, 432, 89, 12),
+        # Just enough input for 24 parallel
+        (VoltageTier.EV, 120, 864, 89, 24),
+        (VoltageTier.EV, 100, 720, 89, 20),
+        (VoltageTier.EV, 60, 1728, 44, 12),
+        # Just enough input for 60 parallel
+        (VoltageTier.UEV, 300, 2_211_840, 2, 60),
+        (VoltageTier.UEV, 290, 2_138_112, 2, 58),
+        (VoltageTier.UEV, 100, 2_949_120, 1, 20),
+        # Just enough input for 66 parallel
+        (VoltageTier.UIV, 330, 9_732_096, 1, 66),
+        # Just enough input for 103 parallel
+        (VoltageTier.UMV, 515, 42_467_328, 1, 103),
     ],
 )
 def test_industrial_centrifuge_black_granite_recipe(
-    machine_voltage, expected_eut, expected_dur, expected_parallel
+    machine_voltage, input_amount, expected_eut, expected_dur, expected_parallel
 ):
     calculator = OverclockCalculator(
         10,
@@ -109,6 +139,8 @@ def test_industrial_centrifuge_black_granite_recipe(
     )
     helper = ParallelHelper(
         calculator,
+        IngredientCollection(Ingredient("black granite dust", input_amount)),
+        recipe_centrifuge_black_granite,
         eut_modifier=0.9,
         max_parallel=6 * VoltageTier(machine_voltage).number(),
     )
@@ -119,3 +151,59 @@ def test_industrial_centrifuge_black_granite_recipe(
     check.equal(expected_eut, res_helper.recipe_voltage)
     check.equal(expected_dur, dur)
     check.equal(expected_parallel, res_helper.parallel)
+
+
+# LCR Recipe: 16x Biotite Dust + 16x Sodium Hydroxide Dust x16 + 4000x Water
+#               -> 64x Sodium Aluminate Dust
+recipe_input_lcr_sodium_al = IngredientCollection(
+    Ingredient("biotite dust", 16),
+    Ingredient("sodium hydroxide dust", 16),
+    Ingredient("water", 4000),
+)
+
+
+@pytest.mark.parametrize(
+    "max_parallel,biotite_amount,sodium_hydroxide_amount,water_amount,expected_parallel",
+    (  # Enough for exactly 1
+        (100, 16, 16, 4000, 1),
+        # Enough for exactly 2
+        (100, 32, 32, 8000, 2),
+        # Enough for exactly 36
+        (100, 576, 576, 144000, 36),
+        # Not enough for 1, missing 1st dust input
+        (100, 15, 16, 4000, 0),
+        # Not enough for 1, missing 2nd dust input
+        (100, 16, 15, 4000, 0),
+        # Not enough for 1, missing fluid input
+        (100, 16, 16, 3999, 0),
+        # Not enough for 1, missing from both dust input
+        (100, 15, 15, 4000, 0),
+        # Not enough for 1, missing something from all input
+        (100, 15, 15, 3999, 0),
+        # more than 1 but not enough for 2 recipe
+        (100, 16, 16, 5000, 1),
+        (100, 20, 20, 5000, 1),
+        # limited by max_parallel
+        (1, 32, 32, 8000, 1),
+        (20, 576, 576, 144000, 20),
+    ),
+)
+def test_max_parallel_calculated_by_inputs(
+    max_parallel,
+    biotite_amount,
+    sodium_hydroxide_amount,
+    water_amount,
+    expected_parallel,
+):
+    calculator = OverclockCalculator(480, 20 * 20, 512)
+    helper = ParallelHelper(
+        calculator,
+        IngredientCollection(
+            Ingredient("biotite dust", biotite_amount),
+            Ingredient("sodium hydroxide dust", sodium_hydroxide_amount),
+            Ingredient("water", water_amount),
+        ),
+        recipe_input_lcr_sodium_al,
+    )
+
+    assert expected_parallel == helper.max_parallel_calculated_by_inputs(max_parallel)
